2ccb48c26bd034360f5cf466dc13405a1f5fbaa1
nicroco <44801950+nqwrc@users.noreply.github.com>
2023-11-18 07:30:19 +0100

    Add files via upload
    build.py is used by pyinstaller

---
+++ build.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import os
+
+def build():
+    script_path = "tidal_migrator_gui.py"  # Path to your main Python script
+    # icon_path = "dove.ico"  # Path to your icon file
+    output_name = "tidal_migrator_0.1.0"
+    
+    # Command to run PyInstaller
+    command = [
+        sys.executable, "-m", "PyInstaller",
+        "--onefile", "--windowed",
+        "--name", output_name,
+        script_path,
+    ]
+
+    # Execute the command
+    subprocess.run(command, check=True)
+
+if __name__ == "__main__":
+    build()
